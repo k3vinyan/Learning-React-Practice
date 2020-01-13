a5f2c7bfe0a3402ed38cde909e89dfb519e65ed1
Makeven Yan <makevenyan@Makevens-MacBook-Pro.local>
2020-01-13 15:59:40 -0800

    clean up code in chapterr4

---
+++ Chapter4/recipes/friedChicken.js
@@ -0,0 +1,36 @@
+const ingredients = [
+    "2 whole free-range, organic chickens",
+    "Vegetable oil of choice, for frying the chicken",
+    "6 cups all-purpose flour",
+    "5 tablespoons salt",
+    "4 tablespoons ground black pepper",
+    "2 tablespoons garlic powder",
+    "1 tablespoon onion powder",
+    "2 teaspoons cayenne pepper",
+    "2 cups buttermilk",
+]
+const steps = [
+    "Cut the whole chickens into 4 breasts, 4 thighs, 4 legs and 4 wings and set aside.",
+    "Preheat your oil, in either a heavy pan on the stove or a deep-fryer, to 325 degrees F.",
+    "In a large bowl, combine the flour, salt, black pepper, garlic powder, onion powder and cayenne pepper until thoroughly mixed. Set aside.",
+    "Pour the buttermilk into another bowl large enough for the chicken to be immersed in the buttermilk.",
+    "Prepare your dredging station. Place your chicken in a bowl. Next to that, your bowl of buttermilk, and next to that, your dry mixture.",
+    "Take your breasts, lightly dust them with your flour mixture, then dip them in the buttermilk until they are coated, and then place them in the flour mixture.",
+    "Take the breasts that are in the flour mixture and aggressively push the flour mixture into the wet chicken. Make sure that the chicken in very thoroughly coated, or you will not achieve the crust and crunch you are looking for. Gently place the breasts in your hot oil.",
+    "Next, repeat the dredging steps with your other pieces of chicken in this order: thigh, leg then wing.",
+    "When you place the last wing into the fryer, you should have 16 pieces of chicken in the oil. Set a timer for 15 minutes.",
+    "After 15 minutes, take a probe thermometer and check the temperature of a breast. If it reads 180 degrees F, all of your chicken is done. (Keep in mind that it will continue to cook after it has been removed from the fryer.)",
+    "Remove the chicken from the oil and let it drain for 5 minutes. Let cool for an additional 10 minutes before serving.",
+    "This recipe was provided by a chef, restaurant or culinary professional. It has not been tested for home use."
+]
+
+ const friedChickenRecipe = {
+    name: "fried Chicken",
+    prepTime: "50 mins",
+    cookTime: "20 mins",
+    steps,
+    ingredients,
+    image: './images/fried-chicken-in-baskets_800.jpg'
+}
+
+export { friedChickenRecipe }
--- Chapter4/script.js
+++ Chapter4/script.js
@@ -1,38 +1,6 @@
+import { friedChickenRecipe } from './recipes/friedChicken.js'
 
-const ingredients = [
-    "2 whole free-range, organic chickens",
-    "Vegetable oil of choice, for frying the chicken",
-    "6 cups all-purpose flour",
-    "5 tablespoons salt",
-    "4 tablespoons ground black pepper",
-    "2 tablespoons garlic powder",
-    "1 tablespoon onion powder",
-    "2 teaspoons cayenne pepper",
-    "2 cups buttermilk",
-]
-const steps = [
-    "Cut the whole chickens into 4 breasts, 4 thighs, 4 legs and 4 wings and set aside.",
-    "Preheat your oil, in either a heavy pan on the stove or a deep-fryer, to 325 degrees F.",
-    "In a large bowl, combine the flour, salt, black pepper, garlic powder, onion powder and cayenne pepper until thoroughly mixed. Set aside.",
-    "Pour the buttermilk into another bowl large enough for the chicken to be immersed in the buttermilk.",
-    "Prepare your dredging station. Place your chicken in a bowl. Next to that, your bowl of buttermilk, and next to that, your dry mixture.",
-    "Take your breasts, lightly dust them with your flour mixture, then dip them in the buttermilk until they are coated, and then place them in the flour mixture.",
-    "Take the breasts that are in the flour mixture and aggressively push the flour mixture into the wet chicken. Make sure that the chicken in very thoroughly coated, or you will not achieve the crust and crunch you are looking for. Gently place the breasts in your hot oil.",
-    "Next, repeat the dredging steps with your other pieces of chicken in this order: thigh, leg then wing.",
-    "When you place the last wing into the fryer, you should have 16 pieces of chicken in the oil. Set a timer for 15 minutes.",
-    "After 15 minutes, take a probe thermometer and check the temperature of a breast. If it reads 180 degrees F, all of your chicken is done. (Keep in mind that it will continue to cook after it has been removed from the fryer.)",
-    "Remove the chicken from the oil and let it drain for 5 minutes. Let cool for an additional 10 minutes before serving.",
-    "This recipe was provided by a chef, restaurant or culinary professional. It has not been tested for home use."
-]
-
-const friedChickenRecipe = {
-    name: "fried Chicken",
-    prepTime: "50 mins",
-    cookTime: "20 mins",
-    steps,
-    ingredients,
-    image: './images/fried-chicken-in-baskets_800.jpg'
-}
+//https://www.foodnetwork.com/recipes/fried-chicken-recipe10-3381583
 
 const recipeImage = ({image}) =>
     React.createElement('img', {className: 'recipe-image', src: image}, null)
@@ -43,41 +11,25 @@ const recipeIngredients = ({ingredients}) =>
             React.createElement('li', { className: 'ingredient-item', key: i }, ingredient)
         )
     )
+
 const recipeName = ({name}) =>
     React.createElement('h1', {className: 'recipe-name'}, name)
 
 const recipeSteps = ({steps}) =>
     React.createElement('ol', {className: 'recipe-list-step'}, 
-        ingredients.map( (step, i) => 
+        steps.map( (step, i) => 
             React.createElement('li', { className: 'step-item', key: i}, step)
         )
     )
-
-
-
-
-
-
-const chickenImage = recipeImage(friedChickenRecipe);
-const chickenName = recipeName(friedChickenRecipe);
-const chickenIngredients = recipeIngredients(friedChickenRecipe);
-const chickenSteps = recipeSteps(friedChickenRecipe);
-
-
-
-const RecipeComponent = (image, name, ingredients, steps) => 
+const RecipeComponent = (recipe) => 
     React.createElement('div', {className: 'recipe-container'}, 
-        image,
-        name,
-        ingredients,
-        steps
+        recipeImage(recipe),
+        recipeName(recipe),
+        recipeIngredients(recipe),
+        recipeSteps(recipe)
     )
 
 
-const chickenComponent = RecipeComponent(chickenImage, chickenName, chickenIngredients, chickenSteps)
-
-console.log(chickenComponent)
-
-    //https://www.foodnetwork.com/recipes/fried-chicken-recipe10-3381583
+const chickenComponent = RecipeComponent(friedChickenRecipe)
 
 ReactDOM.render(chickenComponent, document.getElementById('app'))
