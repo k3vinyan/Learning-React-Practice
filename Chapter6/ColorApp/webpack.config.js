d6d3969bb6c946143f6d5387c579aad49a7601b1
Makeven Yan <makevenyan@Makevens-MacBook-Pro.local>
2020-01-18 15:06:30 -0800

    start new ColorApp for more practice

---
+++ Chapter6/ColorApp/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /(node_modules)/,
+                query: {
+                    presets: ['@babel/preset-env', '@babel/preset-react']
+                }
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist'),
+        compress: true,
+        hot: true,
+        port: 9001
+    },
+    mode: 'development'
+}
